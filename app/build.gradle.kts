782d3245aa41429f7d8c6a5f0bd61539a64f5eb4
SviatKuzbyt <sviatoslav.kuzbyt@gmail.com>
2025-03-28 11:49:12 +0200

    add and config dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,15 @@
 plugins {
+    //core and compose
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //room
+    alias(libs.plugins.google.devtools.ksp)
+    //navigation
+    alias(libs.plugins.kotlin.serialization)
+    //hilt
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -26,6 +34,12 @@ android {
                 "proguard-rules.pro"
             )
         }
+
+        create("profile"){
+            initWith(getByName("debug"))
+            isDebuggable = false
+            isProfileable = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -39,21 +53,37 @@ android {
     }
 }
 
-dependencies {
+kapt {
+    correctErrorTypes = true
+}
 
+dependencies {
+    //Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+
+    //Compose
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
+    //navigation
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.androidx.navigation.compose)
+
+    //hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    kapt(libs.hilt.android.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    //core and compose
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    //room
+    alias(libs.plugins.google.devtools.ksp) apply false
+    //navigation
+    alias(libs.plugins.kotlin.serialization) apply false
+    //Hilt
+    alias(libs.plugins.dagger.hilt) apply false
 }
